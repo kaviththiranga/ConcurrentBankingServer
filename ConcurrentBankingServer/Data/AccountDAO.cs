2748fd45aaa11b79b62857fd61d556f5bbe7dcca
Kavith Thiranga <rc404mekt@gmail.com>
2013-01-22 12:27:47 +0530

    initial Commit

---
+++ ConcurrentBankingServer/Data/AccountDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConcurrentBankingServer.Model;
+
+namespace ConcurrentBankingServer.Data
+{
+    interface AccountDAO
+    {
+        public List<Account> getAccounts();
+
+        public bool athenticate(String accNo, String pin);
+
+        public Account getAccountByAccNo(String accNo);
+    }
+}
