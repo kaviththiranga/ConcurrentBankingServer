8a7f74f7432056fae5a4de428cddce7d5f77422f
Kavith Thiranga <rc404mekt@gmail.com>
2013-01-22 14:31:55 +0530

    Added a delegate

---
+++ ConcurrentBankingServer/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConcurrentBankingServer
+{
+    public class Server
+    {
+        //Delegate to be used in displaying log messages
+        public delegate void Log(String msg);
+
+        protected Log logger;
+
+        public Server() { }
+
+        public Server(Log logger) {
+            this.logger = logger;
+
+        }
+
+    }
+}
